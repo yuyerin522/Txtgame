de964e142468edee5efc49ee5432fde7f259607c
yuyerin <annie7373@naver.com>
2024-04-25 18:13:53 +0900

    00

--- Program.cs
+++ Program.cs
@@ -2,6 +2,30 @@
 
 namespace Txtgame
 {
+
+    class Item //아이템 클래스 .. 이렇게 해주는게 맞나?
+    {
+        public string Name { get; }
+        public int Defense { get; }
+        public int Attack { get; }
+        public string Description { get; }
+        public int Price { get; }
+
+        public Item(string name, int defense, int attack, string description, int price)
+        {
+            Name = name;
+            Defense = defense;
+            Attack = attack;
+            Description = description;
+            Price = price;
+        }
+
+        public string ToFormattedString()
+        {
+            return $"{Name,-15} | 방어력 +{Defense,-2} | 공격력 +{Attack,-2} | {Description,-45} | {Price,5} G";
+        }
+    }
+
     internal class Program
     {
         //캐릭터 스텟은 아이템에 따라 변하기 때문에 변수로 지정
@@ -17,9 +41,20 @@ namespace Txtgame
         static int inventoryCount = 0;              // 현재 인벤토리에 있는 아이템의 개수
         static bool[] equippedItems = new bool[10]; // 장착된 아이템을 추적하는 배열. 인벤토리의 각 아이템에 대해 장착 여부
 
+        static Item[] shopItems = new Item[] //아이템들
+            {
+                new Item("수련자 갑옷", 5, 0, "수련에 도움을 주는 갑옷입니다.", 1000),
+                new Item("무쇠갑옷", 9, 0, "무쇠로 만들어져 튼튼한 갑옷입니다.", 2000),
+                new Item("스파르타의 갑옷", 15, 0, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500),
+                new Item("낡은 검", 0, 2, "쉽게 볼 수 있는 낡은 검 입니다.", 600),
+                new Item("청동 도끼", 0, 5, "어디선가 사용됐던거 같은 도끼입니다.", 1500),
+                new Item("스파르타의 창", 0, 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2000)
+            };
 
-        static void Main (string[] args) //메인화면
-        { 
+
+
+        static void Main(string[] args) //메인화면
+        {
             bool exit = false;
 
             while (!exit)
@@ -53,13 +88,13 @@ namespace Txtgame
                         break;
 
                     default:  //1(4) 잘못선택
-                        Console.WriteLine("잘못된 입력입니다");                      
+                        Console.WriteLine("잘못된 입력입니다");
                         break;
                 }
             }
         }
 
-        static void ShowInfo () //1(1) 상태보기
+        static void ShowInfo() //1(1) 상태보기
         {
             bool exit = false;
 
@@ -81,8 +116,8 @@ namespace Txtgame
 
                 //나가기 멘트
                 Console.WriteLine();
-                Console.WriteLine("0. 나가기");              
-                
+                Console.WriteLine("0. 나가기");
+
                 string choice = Console.ReadLine();
 
                 switch (choice)
@@ -99,12 +134,12 @@ namespace Txtgame
 
         static void ShowInven() //1(2) 인벤토리
         {
-            
+
             bool exit = false;
 
             while (!exit)
             {
-                
+
                 //시작 멘트
                 Console.Clear();
                 Console.WriteLine("인벤토리");
@@ -155,7 +190,7 @@ namespace Txtgame
 
 
             }
-            
+
         }
 
         static void ShowStore() //1(3) 상점
@@ -173,7 +208,10 @@ namespace Txtgame
                 Console.WriteLine("[ 보유골드:" + gold + "G ]");
 
 
-
+                for (int i = 0; i < shopItems.Length; i++) //아이템 배열
+                {
+                    Console.WriteLine($"{i + 1}. {shopItems[i].Name,-15} | 방어력 {shopItems[i].Defense,-3} | 공격력 {shopItems[i].Attack,-3} | {shopItems[i].Description,-45} | {shopItems[i].Price,5} G");
+                }
 
 
 
@@ -188,7 +226,7 @@ namespace Txtgame
                 string choice = Console.ReadLine();
 
                 switch (choice)
-                {                
+                {
                     case "0":
                         exit = true; // 0을 입력하면 나가기
                         break;
@@ -219,7 +257,7 @@ namespace Txtgame
                     Console.WriteLine($"{i + 1}. {equippedStatus}{inventory[i]}");
                 }
 
-                
+
 
 
 
@@ -233,14 +271,15 @@ namespace Txtgame
                 string choice = Console.ReadLine();
 
                 switch (choice)
-                {                   
+                {
                     case "0":
                         exit = true; // 0을 입력하면 나가기
-                        break;                  
-                }  //메롱
+                        break;
+                }
             }
         }
 
 
     }
 }
+
